9d4c836fdbab6f80b9157a86fd586c71246d7de6
Dana <dana.said-housseine@etu.u-pec.fr>
2026-06-17 16:41:02 +0200

    feat(s4): ajout validation csv et tests clients

--- service4_csv_mysql/app.py
+++ service4_csv_mysql/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pandas as pd
 import io
 
 app = Flask(__name__)
+CORS(app)
 
 COLONNES_REQUISES = {'nom_serie', 'valeur'}
 
